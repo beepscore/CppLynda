c04c65bff5c7a01af655c5a81c35e4749a59e4d4
beepscore <support@beepscore.com>
2013-03-12 20:26:03 -0700

    In workspace add Ch02.proj as OSX command line tool C++

---
+++ Ch02/Ch02/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ch02
+//
+//  Created by Steve Baker on 3/12/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
